2375aed2965ff5c5a37d5e2e6fb9532c2ee7ec18
Saran-J-2002 <2022wb86804@wilp.bits-pilani.ac.in>
2026-09-24 18:54:34 +0530

    Update output message in Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from 2022WB86804");
+        System.out.println("Continuous integration is working");
     }
 }
